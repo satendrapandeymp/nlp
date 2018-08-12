cd68a82dfb951102cf92e37cc7a6f00ca848396e
Satyendra Pandey <satyendrapandeyiitk@gmail.com>
2018-08-13 04:16:21 +0530

    Update langProcessing.py

--- langProcessing.py
+++ langProcessing.py
@@ -14,18 +14,18 @@ import re, math, time
 import numpy as np
 
 
-# Defining some important class
+# Defining some important things for future use
 stop_words = set(stopwords.words('english'))
 stemmer = SnowballStemmer("english")
 WORD = re.compile(r'\w+')
 
 
-# To get column
+# To get column of a 2d matrix it have been used in row to column transform 
 def column(matrix, i):
     return [row[i] for row in matrix]
 
 
-# To get word to vec
+# To get word to vec well you can also call it word counter
 def w2vec(data):
 	temp = Counter(data)
 	return temp	
@@ -39,7 +39,7 @@ def l2doc(arr):
 	return res
 
 
-# to get cosine similarity
+# to get cosine similarity of two arrays
 def get_cosine(doc1, doc2):
 	vec1 = w2vec(doc1)
 	vec2 = w2vec(doc2)
@@ -63,7 +63,7 @@ def subsParse(filename):
 	return result
 
 
-# for tutorial parsing
+# for tutorial parsing into different sections
 def docParse(filename):
 	result = []
 	with open(filename, 'r') as tempReader:
@@ -81,13 +81,20 @@ def docParse(filename):
 
 # to preprocess the data
 def preProcess(mess):
+	# Converting to lower
 	mess = mess.lower()
+	# removing numbers other than alpha-numeric
 	mess = WORD.findall(mess)
 	mess = " ".join(mess)
+	# tokenizing words
 	word_tokens = word_tokenize(mess)
+	# Removing stop_words
 	word_tokens = [temp for temp in word_tokens if temp not in stop_words]
+	# giving pos_tags and removing some pos
 	pos = pos_tag(word_tokens, tagset='universal')
+	# stemming the words
 	pos = [stemmer.stem(word) for (word, tag) in pos if tag in ['NOUN', 'X']]
+	# removing common words from youtube tutorials to reduce noise in result
 	pos = [tword for tword in pos if tword not in cwords]
 	string = " ".join(pos)
 	return string, pos
@@ -95,6 +102,7 @@ def preProcess(mess):
 
 # finding most common words from tutorial to remove that
 # Like in youtube you'll commonly hear this term like guys, share, like, enjoy...
+# give filenames as input, just download tutorials on some other topics too to find the useful common words
 def commonAll(ytFiles):
 	wlist = []
 	for ytFile in ytFiles:
@@ -105,7 +113,7 @@ def commonAll(ytFiles):
 	return temp	
 
 
-# Already assign common word to be loaded
+# Already assign common word to be loaded which is in common.txt
 def commonSpec(fileName):
 	with open(fileName, 'r') as tempReader:
 		mess = tempReader.read()
